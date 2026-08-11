fd09212fe41b5b13678a09e83742d3e9a0a6113b
notzer <poudeloshan111@gmail.com>
2026-08-11 21:08:39 +0545

    Initial commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDRHmvYcFb6jzPAWrtK6udMZ8mCc8y39w0",
-  authDomain: "myselfoshan.firebaseapp.com",
-  projectId: "myselfoshan",
-  storageBucket: "myselfoshan.firebasestorage.app",
-  messagingSenderId: "808907746677",
-  appId: "1:808907746677:web:228643fd7924f51d20f73c",
-  measurementId: "G-DG6CLVCW1R"
+export const firebaseConfig={
+apiKey:"AIzaSyDRHmvYcFb6jzPAWrtK6udMZ8mCc8y39w0",
+authDomain:"myselfoshan.firebaseapp.com",
+projectId:"myselfoshan",
+storageBucket:"myselfoshan.firebasestorage.app",
+messagingSenderId:"808907746677",
+appId:"1:808907746677:web:228643fd7924f51d20f73c",
+measurementId:"G-DG6CLVCW1R"
 };
